3b72207468319b68a2f4b0b871221c9ac6d46feb
samatchi <youssefsindel@gmail.com>
2022-11-08 23:00:24 +0100

    Add future flag to use new interface

--- backend/database.py
+++ backend/database.py
@@ -9,7 +9,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///somefile.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, future=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
